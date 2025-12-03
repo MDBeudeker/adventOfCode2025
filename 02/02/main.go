868efb9f0c4ed55a1ebfedd0e222cbd8dbfe697e
MDBeudeker <mauritsbeu@gmail.com>
2025-12-03 15:38:35 +0100

    ex2-02 prettify

--- 02/02/main.go
+++ 02/02/main.go
@@ -19,14 +19,11 @@ func main() {
 	check(err)
 	values := strings.Split(string(dat[:]), ",")
 	var total int64 = 0
-	// var buffer bytes.Buffer
 
 	fmt.Println(values)
 	for i := range len(values) {
 		total += checkDoubles(values[i])
-		// buffer.WriteString(checkDoubles(values[i]))
 	}
-	// fmt.Println(buffer.String())
 	fmt.Printf("total is %#v", total)
 }
 
@@ -41,8 +38,6 @@ func checkDoubles(value string) int64 {
 	for i := start; i <= end; i++ {
 		doubleDetected := detectDoubles(i)
 		if doubleDetected {
-			// fmt.Printf("hoi %#v\n", i)
-			// return (strconv.Itoa(i))
 			// fmt.Println("doubles detected")
 			total += i
 		}
@@ -86,35 +81,16 @@ func stripInt(i int64, length int64, j int64) bool {
 	orderOfMagnitude := math.Pow(10, float64(length)/float64(j))
 	diminisher := float64(math.Pow(orderOfMagnitude, float64(j-1)))
 	repeater := math.Floor(float64(value) / diminisher)
-	// if i == 2121212121 {
 
-	// 	fmt.Printf("%#v has %#v parts with order of magnitude %#v and repeats %#v\n", i, j, orderOfMagnitude, repeater)
-
-	// }
 	remainder := value
 	for k := 0; k <= int(j); k++ {
 		remainder = value - int64(repeater)*int64(diminisher)
-		// if i == 2121212121 {
-		// fmt.Printf("%#v minus %#v is %#v\n", value, int64(repeater)*int64(diminisher), remainder)
-		// fmt.Printf("dividing %#v by  %#v\n", (float64(value - int64(repeater))), orderOfMagnitude)
-		// }
 		diminisher = diminisher / orderOfMagnitude
-		// fmt.Printf("old value %#v and remainder %#v\n", value, remainder)
 		value = remainder
-		// fmt.Printf("new value %#v and remainder %#v\n", value, remainder)
-		// if value < 0 {
-		// 	return false
-		// }
+
 		if k == int(j) && value == 0 {
-			// fmt.Printf("%#v\n", i)
 			return true
 		}
 	}
-	return false
-	// if value == 282828 || value == 2828 || value == 28 {
-	// 	fmt.Printf("dividing %#v by  %#v\n", (float64(i)), diminisher)
-	// 	fmt.Printf("%#v minus %#v is %#v\n", i, int64(repeater)-int64(repeater*orderOfMagnitude), remainder)
-	// 	fmt.Printf("%#v has %#v parts with order of magnitude %#v and repeats %#v, remainder is %#v\n", i, j, orderOfMagnitude, repeater, remainder)
-	// }
 	return false
 }
